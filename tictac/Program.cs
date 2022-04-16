65f91e5911df2c68eac80d1559cb381eca984830
Xavier CAMIER <camierxcom@icloud.com>
2022-04-16 09:43:10 +0200

    Introduction of Mudblazor and rework of the menus

--- tictac/Program.cs
+++ tictac/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using tictac.Areas.Identity;
 using tictac.Data;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
